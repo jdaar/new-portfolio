4b0f7131ff30782507b05838dfad44341701a710
Jhonatan David <jhonatandaar@gmail.com>
2023-08-06 12:03:54 -0500

    fix: server side route makes buggy build

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,9 +1,7 @@
-import type { LayoutServerLoad } from './$types';
-
 export const ssr = false;
 export const csr = true;
 export const prerender = false;
 
 export const load = (async () => {
-	return {};
-}) satisfies LayoutServerLoad;
+    return {};
+}) 
