1e7aa29e0855a9d52d6c118cd86329d2ae021b17
Jhonatan David <jhonatandaar@gmail.com>
2025-02-05 21:23:01 -0500

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -13,6 +13,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		paths: {
 			base: '/new-portfolio',
+			relative: true,
 			assets: 'https://jdaar.github.io/new-portfolio'
 		},
 		adapter: adapter({
