ec1dab0dd253f82dd9536289a0ba93c404633fbf
Jhonatan David <jhonatandaar@gmail.com>
2025-02-05 21:18:43 -0500

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,7 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		paths: {
-			base: 'new-portfolio',
+			base: 'new-portfolio/',
 			assets: 'https://jdaar.github.io/new-portfolio'
 		},
 		adapter: adapter({
