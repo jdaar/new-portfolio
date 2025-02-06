e6a729605b22ae4c244041fe8fa2ba84bdab3b61
Jhonatan David <jhonatandaar@gmail.com>
2025-02-05 21:06:39 -0500

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		paths: {
 			base: '',
-			assets: 'https://jhonatan.tech'
+			assets: 'https://jdaar.github.io/new-portfolio'
 		},
 		adapter: adapter({
 			pages: 'build',
